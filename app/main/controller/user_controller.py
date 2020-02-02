c1ab1c4a0780993a3ebb5f6cb2616824e88604b4
KJ Wilson <yourboykjdev@gmail.com>
2020-02-02 02:24:11 -0800

    finished user model

--- app/main/controller/user_controller.py
+++ app/main/controller/user_controller.py
@@ -2,13 +2,10 @@ from flask import request
 from flask_restplus import Resource
 
 from ..util.dto import UserDto
-from ..util.dto import BudgetDto
 from ..service.user_service import save_new_user, get_all_users, get_a_user
-from ..service.budget_service import save_new_budget, get_all_budgets, get_all_user_budgets
 
 api = UserDto.api
 _user = UserDto.user
-_budget = BudgetDto.budget
 
 @api.route('/')
 class UserList(Resource):
--- app/main/model/user.py
+++ app/main/model/user.py
@@ -1,19 +1,24 @@
 from .. import db, flask_bcrypt
 import datetime
-from app.main.model.blacklist import BlacklistToken
 from ..config import key
-import jwt
+# from app.main.model.blacklist import BlacklistToken
+# import jwt
 
 # User class inherits from db.Model class which declares the class as a model for sqlalchemy
 class User(db.Model):
-    """ User Model for storing user related details """
+    """ 
+    User Model for storing user related details
+    """
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # password_hash = db.Column(db.String(100))
 
-    # User Authentication fields
+    # User Contact fields
     email = db.Column(db.String(255), unique=True, nullable=False)
-    username = db.Column(db.String(50), unique=True)
-    password_hash = db.Column(db.String(100))
+    phone = db.Column(db.String(10), unique=True)
+    address = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    zip_code = db.Column(db.String(5))
 
     # User fields
     registered_on = db.Column(db.DateTime, nullable=False)
@@ -22,61 +27,68 @@ class User(db.Model):
     lname = db.Column(db.String(100))
     admin = db.Column(db.Boolean, nullable=False, default=False)
 
-    # Relationships
-    budgets = db.relationship("Budget", lazy='select', backref=db.backref("user.public_id", lazy='joined'))
+    """
+    Store
+    """
+    # One to One
+    # TODO: Closet
+    # TODO: Cart
 
-    @property
-    def password(self):
-        raise AttributeError('password: write-only field')
-
-    # password.setter uses flask-bcrypt to generate a hash using the provided password.
-    @password.setter
-    def password(self, password):
-        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
-
-    def check_password(self, password):
-        return flask_bcrypt.check_password_hash(self.password_hash, password)
-
+    # One to Many
+    orders = db.relationship("Order", lazy='select', backref=db.backref("user.public_id", lazy='joined'))
+    
     def __repr__(self):
         return "<User '{}'>".format(self.username)
 
-    @staticmethod
-    def encode_auth_token(user_id):
-        """
-        Generates the Auth Token
-        :return: string
-        """
-        try:
-            payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
-                'iat': datetime.datetime.utcnow(),
-                'sub': user_id
-            }
-            return jwt.encode(
-                payload,
-                key,
-                algorithm='HS256'
-            )
-        except Exception as e:
-            return e
+    # @property
+    # def password(self):
+    #     raise AttributeError('password: write-only field')
 
-    @staticmethod
-    def decode_auth_token(auth_token):
-        """
-        Decodes the auth token
-        :param auth_token:
-        :return: integer|string
-        """
-        try:
-            payload = jwt.decode(auth_token, key)
-            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
-            if is_blacklisted_token:
-                return 'Token blacklisted. Please log in again.'
-            else:
-                # returns user_id
-                return payload['sub']
-        except jwt.ExpiredSignatureError:
-            return 'Signature expired. Please log in again.'
-        except jwt.InvalidTokenError:
-            return 'Invalid token. Please log in again.'
+    # password.setter uses flask-bcrypt to generate a hash using the provided password.
+    # @password.setter
+    # def password(self, password):
+    #     self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+
+    # def check_password(self, password):
+    #     return flask_bcrypt.check_password_hash(self.password_hash, password)
+
+    # @staticmethod
+    # def encode_auth_token(user_id):
+    #     """
+    #     Generates the Auth Token
+    #     :return: string
+    #     """
+    #     try:
+    #         payload = {
+    #             'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
+    #             'iat': datetime.datetime.utcnow(),
+    #             'sub': user_id
+    #         }
+    #         return jwt.encode(
+    #             payload,
+    #             key,
+    #             algorithm='HS256'
+    #         )
+    #     except Exception as e:
+    #         return e
+
+    # @staticmethod
+    # def decode_auth_token(auth_token):
+    #     """
+    #     Decodes the auth token
+    #     :param auth_token:
+    #     :return: integer|string
+    #     """
+    #     try:
+    #         payload = jwt.decode(auth_token, key)
+    #         is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+    #         if is_blacklisted_token:
+    #             return 'Token blacklisted. Please log in again.'
+    #         else:
+    #             # returns user_id
+    #             return payload['sub']
+    #     except jwt.ExpiredSignatureError:
+    #         return 'Signature expired. Please log in again.'
+    #     except jwt.InvalidTokenError:
+    #         return 'Invalid token. Please log in again.'
             
