ab36a228b75ba18604e8605266e1b6841229f103
KJ Wilson <yourboykjdev@gmail.com>
2020-02-07 02:09:34 -0800

    drafted cart controller

---
+++ app/main/controller/cart_controller.py
@@ -0,0 +1,31 @@
+from flask import request
+from flask_restplus import Resource
+
+from ..util.dto import CartDto
+from ..service.cart_service import get_a_cart, get_all_carts, empty_cart
+
+api = CartDto.api
+_cart = CartDto.cart
+
+@api.route('/carts')
+class CartList(Resource):
+    @api.doc('list of carts')
+    @api.marshal_list_with(_cart, envelope='data')
+    def get(self):
+        """List all budgets"""
+        return get_all_carts()
+
+@api.route('/<public_id>/cart')
+@api.param('public_id', 'The User identifier')
+@api.response(404, 'User not found.')
+class Cart(Resource):
+    @api.doc('get a user')
+    @api.marshal_with(_user)
+    def get(self, public_id):
+        """get a user given its identifier"""
+        user = get_a_user(public_id)
+        if not user:
+            api.abort(404)
+        else:
+            return user.cart
+            # return user.cart.items
---
+++ app/main/controller/routes.md
@@ -0,0 +1,53 @@
+# Routes
+
+## User Methods
+
+---
+
+## /user/
+
+**GET** /user/
+
+returns list of users
+in order of when they registered
+
+**POST** /user/
+
+creates new user
+returns response_object, 201
+
+```python
+response_object = {
+    'status': 'success',
+    'message': 'Successfully registered.',
+    'authorization': auth_token.decode(),
+    'public_id': user.public_id,
+    'cart': user.cart
+}
+```
+
+## /user/public_id
+
+**GET** /user/public_id
+
+returns user
+
+## Cart Methods
+
+---
+
+## /user/carts
+
+**GET** /user/carts
+
+returns all carts
+
+**POST** /user/carts
+
+no post method for carts
+carts are instantiated with create_user
+
+## /user/public_id/cart
+
+currently returns cart
+should return cart.items
