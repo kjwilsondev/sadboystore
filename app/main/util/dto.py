73656b4c14786d1aa88b348faf926cd9894b9b74
KJ Wilson <yourboykjdev@gmail.com>
2020-02-09 23:41:45 -0800

    added item dto

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -17,12 +17,23 @@ class UserDto:
         'phone': fields.String(required=False, description='phone number'),
         'address': fields.String(required=False, description='user address'),
         'city': fields.String(required=False, description='city'),
-        'zip_code': fields.String(required=False, description='zip code')
+        'zip_code': fields.Integer(required=False, description='zip code')
     })
 
 class CartDto:
     api = Namespace('cart', description='cart operations')
     cart = api.model('cart', {
         'user_id': fields.String(required=True, description='cart user id'),
-        'cost': fields.String(required=True, description='cart cost')
+        'cost': fields.Float(required=True, description='cart cost')
+    })
+
+class ItemDto:
+    api = Namespace('item', description='item operations')
+    item = api.model('item', {
+        'name': fields.String(required=True, description='item name'),
+        'piece': fields.String(required=True, description='item piece'),
+        'cost': fields.Float(required=True, description='item cost'),
+        'color': fields.String(required=True, description='item color'),
+        'size': fields.String(required=True, description='item size'),
+        'available': fields.Integer(required=True, description='item available'),
     })
